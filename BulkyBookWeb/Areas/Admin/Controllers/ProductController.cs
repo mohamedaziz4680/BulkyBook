34eb72a70a2b8cd968ac57fc71281444a4dcd7f6
Mohamed Aziz <mohamed.aziz.4680@gmail.com>
2022-12-29 17:08:09 +0200

    adding product model with controller and his repository

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    [Area("Admin")]
+
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            
+            if (ModelState.IsValid) {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+        }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            var product = _unitOfWork.Product.GetFirstOrDefault(ele => ele.Id== id);
+            if (product == null) { return NotFound(); }
+            return View(product);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid) {
+                _unitOfWork.Product.Update(obj);
+            _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully";
+
+                return RedirectToAction("Index");
+        }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            var product = _unitOfWork.Product.GetFirstOrDefault(ele => ele.Id == id);
+            if (product == null) { return NotFound(); }
+            return View(product);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var product = _unitOfWork.Product.GetFirstOrDefault(ele => ele.Id == id);
+            if (product == null) { return NotFound(); }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
