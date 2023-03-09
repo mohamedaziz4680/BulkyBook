8a2b546a7fcc3715230a5f012d2db769a0786495
Mohamed Aziz <mohamed.aziz.4680@gmail.com>
2023-03-09 13:50:00 +0200

    #

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -22,42 +24,43 @@ namespace BulkyBookWeb.Controllers
         }
 
         //GET
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            return View();
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Product obj)
-        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.ID.ToString(),
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                })
+            };
+            if (id == null || id == 0) { 
+                // create product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewBag.CoverTypeList = CoverTypeList;
+                return View(productVM);
+            }else
+            {
+                // update product
+            }
             
-            if (ModelState.IsValid) {
-                _unitOfWork.Product.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product Created Successfully";
-                return RedirectToAction("Index");
-        }
-            return View(obj);
-        }
-        //GET
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0) { return NotFound(); }
-            var product = _unitOfWork.Product.GetFirstOrDefault(ele => ele.Id== id);
-            if (product == null) { return NotFound(); }
-            return View(product);
+            return View(productVM);
         }
 
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Product obj)
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
         {
             if (ModelState.IsValid) {
-                _unitOfWork.Product.Update(obj);
-            _unitOfWork.Save();
+                //_unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Product Updated Successfully";
 
                 return RedirectToAction("Index");
